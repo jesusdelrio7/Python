12d22d55580250e2a7097446034c93b63c9d4991
jesusdelrio7 <jesusdlrio7@gmail.com>
2019-06-18 20:42:17 -0500

    Add files via upload

---
+++ Clase Python/Clase 1/Ejercicios_18062019.py
@@ -0,0 +1,25 @@
+#Ejercicio 1
+a=float(input('Dame densidad en unidades (kg/m^3):'))
+b=float(input('Dame la altura de la columna en unidades (m):'))
+c=float(9.81)
+p=a*b*c
+print('La presión es ', p,' Pascales')
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    
