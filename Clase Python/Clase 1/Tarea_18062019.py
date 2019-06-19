965196806eae06d6b43822fddda97d694542b502
jesusdelrio7 <51387186+jesusdelrio7@users.noreply.github.com>
2019-06-18 22:30:37 -0500

    subi la tarea

--- Clase Python/Clase 1/Tarea_18062019.py
+++ Clase Python/Clase 1/Tarea_18062019.py
@@ -1,10 +1,61 @@
-#Ejercicio 1
-a=float(input('Dame densidad en unidades (kg/m^3):'))
-b=float(input('Dame la altura de la columna en unidades (m):'))
-c=float(9.81)
-p=a*b*c
-print('La presión es ', p,' Pascales')
+#Tarea 1: Sacar el promedio de la edad de una poblacion registrada en un archivo.c
+import csv
+with open('Tarea_18062019.csv') as archivo:
+    lector=csv.reader(archivo)
 
+    total=0
+    contador=0
+
+    x=input('Quiere promedio de edad o peso?: ')
+
+    next (lector)
+
+    if x=='edad':
+
+#el for va de renglon por renglon
+        for renglon in lector:
+            #renglon [1] sgnifica que este en la columna 1,
+            #como si fuera un arreglo y solo queremos la casilla 1
+            total+= float(renglon[1])
+            contador+=1
+
+        prom=float(total/contador)
+        print('El promedio de edad es',prom)
+
+    if x=='peso':
+        
+        for renglon in lector:
+            total+= float(renglon[2])
+            contador+=1
+
+        prom=float(total/contador)
+        print('El promedio de peso es', prom)
+
+
+        
+        
+
+        
+
+
+
+       
+        
+        
+        
+    
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
 
 
 
